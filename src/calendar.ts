03ebd2792bd9e766238d4158336edd2d92272e0a
Igor Ramadas <igor@devv.com>
2024-06-15 23:00:12 +0200

    New Calendar functions.

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,21 +1,46 @@
 // Strautomator Functions: Calendar
 
+import {CalendarData} from "strautomator-core"
 import core = require("strautomator-core")
 import logger from "anyhow"
 import dayjs from "dayjs"
+import _ from "lodash"
 const settings = require("setmeup").settings
 
 /**
- * Cleanup old cached calendars.
+ * Regenerate expired calendars.
  */
-export const cleanup = async () => {
-    logger.info("F.Calendar.cleanup.start")
+export const regenerate = async () => {
+    logger.info("F.Calendar.regenerate.start")
 
     try {
-        const maxDate = dayjs.utc().subtract(settings.users.idleDays.default, "days")
-        const counter = await core.calendar.deleteCache(maxDate.toDate())
-        logger.info("F.Calendar.cleanup", `Deleted ${counter} cached calendars`)
+        const processCalendar = async (calendar: CalendarData) => {
+            const user = await core.users.getById(calendar.userId)
+
+            // Stop here if user is suspended.
+            if (user.suspended) {
+                logger.debug("F.Calendar.regenerate", core.logHelper.user(user), `User is suspended, skipping calendar ${calendar.id}`)
+                return
+            }
+
+            // Make sure Strava tokens are valid, to avoid triggering multiple refreshes when we are fetching
+            // the user's clubs and activities to build the calendar.
+            if (user.stravaTokens?.expiresAt <= dayjs().unix()) {
+                user.stravaTokens = await core.strava.refreshToken(user.stravaTokens.refreshToken, user.stravaTokens.accessToken)
+            }
+
+            // Here we go!
+            await core.calendar.generate(user, calendar)
+        }
+
+        // Fetch pending calendars and regenerate all of them, in small batches.
+        const pendingCalendars = _.shuffle(await core.calendar.getPendingUpdate())
+        while (pendingCalendars.length) {
+            await Promise.allSettled(pendingCalendars.splice(0, settings.functions.batchSize).map(processCalendar))
+        }
+
+        logger.info("F.Calendar.regenerate", `Updated ${pendingCalendars.length || "no"} calendars`)
     } catch (ex) {
-        logger.error("F.Calendar.cleanup", ex)
+        logger.error("F.Calendar.regenerate", ex)
     }
 }
